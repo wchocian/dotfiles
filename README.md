5d6875ecc73cdc16b38f2527e221ac2232eb0198
wchocian <01169013@pw.edu.pl>
2024-01-16 00:05:11 +0100

    Upload README.md and first sketch of a config for nvim

---
+++ README.md
@@ -0,0 +1,7 @@
+# Wiktor's dotfiles
+
+<!-- ## Installation
+
+### Using Git and the bootstrap script
+
+## Feedback -->
---
+++ nvim/init.lua
@@ -0,0 +1,69 @@
+print("Hello from init.lua")
+
+-- ------------------------------
+-- --------LUA-FUNCTIONS---------
+-- ------------------------------
+function getLineText()
+	local current_line = vim.fn.line(".")
+	local text = vim.fn.getline(current_line)
+	local text = string.sub(text, 1, 6)
+	-- print(current_line, text)
+	return text
+end
+
+-- ------------------------------
+-- -------VIM-SET-OPTIONS--------
+-- ------------------------------
+vim.opt.nu = true
+vim.opt.rnu = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smarttab = true
+vim.opt.mouse = "a"
+vim.opt.incsearch = true
+vim.opt.termguicolors = false
+vim.cmd("tab all")
+vim.cmd("syntax on")
+vim.cmd[[colorscheme dracula]]
+
+-- ------------------------------
+-- ---------VIM-MAPPINGS---------
+-- ------------------------------
+vim.g.mapleader = "<Space>"
+
+vim.keymap.set("n", "<leader-ww>", vim.cmd(":tabnext"), { remap = true })
+vim.keymap.set("n", "<F4>", ":nohl<Cr>", { silent = true })
+
+
+if vim.bo.filetype == 'tex' then
+	vim.keymap.set("v", "<C-b>", "c\\textbf{}<Esc><S-p>")
+	vim.keymap.set("v", "<C-i>", "c\\textit{}<Esc><S-p>")
+end
+
+if vim.bo.filetype == 'lua' then
+	vim.keymap.set("n", "<C-_>", function () 
+		if string.find(getLineText(), '%-%-%s') ~= nil then 
+			vim.cmd(":normal ma^v2lx")
+			vim.cmd(":normal `a3h")
+		else 
+			vim.cmd(":normal ma^i-- ") 
+			vim.cmd(":normal `a3l") 
+		end
+	end)
+	vim.keymap.set("n", "<C-l>", function () getLineText() end)
+end
+
+-- ------------------------------
+-- ---------VIM-PLUGINS----------
+-- ------------------------------
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+	-- Mofiqul's dracula theme
+	use 'Mofiqul/dracula.nvim'
+end)
